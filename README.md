0fc7905d304eda7dddf37da2c2746abfdac3c30b
yosefha4 <yosefha4@ac.sce.ac.il>
2024-01-08 13:29:28 +0200

    Fix HTML next js issue

--- README.md
+++ README.md
@@ -36,7 +36,7 @@ The easiest way to deploy your Next.js app is to use the [Vercel Platform](https
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
 
 
-## Tech Stack
+## Tech Stacks
 
 -Next.js
 -Clerk Auth
---
+++ src/app/api/create-chat/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from "next/server";
+
+// api/create-chat
+
+
+export async function POST(req: Request, res: Response) {
+  try {
+    const body = await req.json();
+    const [file_key,file_name]  = body
+    console.log(file_key,file_name)
+    return NextResponse.json({message:'Success!'})
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
